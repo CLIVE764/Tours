4b1a9ce835159dd6323f0e8edbf4ba81fc02f21c
CLIVE764 <munyamaclive3@gmail.com>
2023-07-20 15:36:04 +0200

    final wdit

--- src/App.jsx
+++ src/App.jsx
@@ -9,11 +9,11 @@ function App() {
 
   return (
 <div className="App">
-  <nav className="flex items-center justify-between py-4 px-8">
-    <img src={img5} alt="" className="w-12 h-12" />
+  <nav className="flex items-center py-4 px-8">
+    <img src={img5} alt="" className="w-10 h-10 rounded-10" />
     <h1 className="text-blue text-xl font-bold">Hello, Dustin</h1>
   </nav>
-  <p className="italic text-center my-4">
+  <p className="italic my-4">
     Answer the questions in chat, and we will find a tour based on your interests
   </p>
   <div className="App-text flex items-center justify-between px-8">
@@ -21,9 +21,9 @@ function App() {
     <p className="text-blue">See All</p>
   </div>
   <div className="pic-thumbnail flex items-center justify-between px-8">
-    <img src={img1} alt="" className="w-32 h-32" />
-    <img src={img2} alt="" className="w-32 h-32" />
-    <img src={img3} alt="" className="w-32 h-32" />
+    <img src={img1} alt="" className=" rounded-t-lg rounded-b-lg w-1200" />
+    <img src={img2} alt="" className="rounded-t-lg rounded-b-lg w-1200" />
+    <img src={img3} alt="" className="rounded-b-lg rounded-t-lg w-1200" />
   </div>
   <div className="App-text2 flex items-center justify-between px-8">
     <h2 className="text-2xl font-bold">Based on your location</h2>
@@ -35,8 +35,8 @@ function App() {
       See Map
     </a>
   </div>
-  <div className="map">
-    <img src={img4} alt="" className="w-full" />
+  <div className="map w-96 h-60 mx-auto">
+    <img src={img4} alt="" className="rounded-t-lg rounded-b-lg w-full" />
   </div>
 </div>
   )
